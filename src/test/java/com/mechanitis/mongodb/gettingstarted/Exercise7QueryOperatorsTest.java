87539763d96c4edced1f1808a7a4a6e876bed719
mgackowski <mwgackowski@gmail.com>
2018-02-09 11:34:50 +0000

    Exercise 7 Pass

--- src/test/java/com/mechanitis/mongodb/gettingstarted/Exercise7QueryOperatorsTest.java
+++ src/test/java/com/mechanitis/mongodb/gettingstarted/Exercise7QueryOperatorsTest.java
@@ -3,6 +3,7 @@ package com.mechanitis.mongodb.gettingstarted;
 import com.mechanitis.mongodb.gettingstarted.person.Address;
 import com.mechanitis.mongodb.gettingstarted.person.Person;
 import com.mechanitis.mongodb.gettingstarted.person.PersonAdaptor;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
@@ -33,10 +34,8 @@ public class Exercise7QueryOperatorsTest {
         collection.insert(PersonAdaptor.toDBObject(bob));
 
         // When
-        //TODO build up a query which checks the numeric value
-        DBObject query = null;
-        // TODO use this query to get a List of matching Documents from the database
-        DBCursor results = null;
+        DBObject query = new BasicDBObject("address.phone", new BasicDBObject("$lt", 1000000000));
+        DBCursor results = collection.find(query);
 
         // Then
         assertThat(results.size(), is(1));
